6e1d01507b4c39a7c07753959280962db0d4c8de
Pisoth Yi <63974224+pisothyi@users.noreply.github.com>
2021-07-02 15:41:26 +0700

    Update firebaseApp.js

--- src/firebase/firebaseApp.js
+++ src/firebase/firebaseApp.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 import "firebase/auth";
 const firebaseApp = firebase.initializeApp({
-  apiKey: REACT_APP_API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "project-zero-vid.firebaseapp.com",
   projectId: "project-zero-vid",
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_SENDER_ID,
-  appId: REACT_APP_APP_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 });
 
 const db = firebase.firestore();
