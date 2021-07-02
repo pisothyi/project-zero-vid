1c4d957253fa12c5c7d9ab743277c7e9a50e420a
Pisoth Yi <63974224+pisothyi@users.noreply.github.com>
2021-07-02 15:40:09 +0700

    Update firebaseApp.js

--- src/firebase/firebaseApp.js
+++ src/firebase/firebaseApp.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 import "firebase/auth";
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCSHbgY_vn43q_LODQXcJCdMRiJB_l3vGQ",
+  apiKey: REACT_APP_API_KEY,
   authDomain: "project-zero-vid.firebaseapp.com",
   projectId: "project-zero-vid",
-  storageBucket: "project-zero-vid.appspot.com",
-  messagingSenderId: "1053236613935",
-  appId: "1:1053236613935:web:68a478966653de22bcc7da",
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_SENDER_ID,
+  appId: REACT_APP_APP_ID,
 });
 
 const db = firebase.firestore();
